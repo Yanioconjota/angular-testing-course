42e1d7f1257e236d39522586357c0981c283fafc
Janio Isacura <janioisacura@gmail.com>
2022-08-11 16:06:41 -0300

    home container component UT updated

--- src/app/courses/home/home.component.spec.ts
+++ src/app/courses/home/home.component.spec.ts
@@ -21,10 +21,16 @@ describe('HomeComponent', () => {
   let fixture: ComponentFixture<HomeComponent>;
   let component:HomeComponent;
   let el: DebugElement;
+  let coursesService: any;
+
+  //we call the findAllCourses method, it returns an Observable<Course[]>, and we mock the result by using the setupCourses() utility and filter the response by category
+  const beginnerCourses = setupCourses().filter(course => course.category === 'BEGINNER');
+
+  const advancedCourses = setupCourses().filter(course => course.category === 'ADVANCED');
 
   beforeEach(waitForAsync(() => {
 
-    const coursesServiceSpy = jasmine.createSpyObj('CoursesService', ['findAllCourdes']);
+    const coursesServiceSpy = jasmine.createSpyObj('CoursesService', ['findAllCourses']);
 
     TestBed.configureTestingModule({
       imports: [
@@ -40,6 +46,8 @@ describe('HomeComponent', () => {
         fixture = TestBed.createComponent(HomeComponent);
         component = fixture.componentInstance;
         el = fixture.debugElement;
+        //mock injection of our service to access its methods
+        coursesService = TestBed.inject(CoursesService);
       })
 
   }));
@@ -53,29 +61,69 @@ describe('HomeComponent', () => {
 
   it("should display only beginner courses", () => {
 
-    pending();
-
+    //we use the 'of' operator of rxjs to let know that the expected response is an Observable<Course[]>
+    coursesService.findAllCourses.and.returnValue(of(beginnerCourses));
+    //we let know the component that the data is passed and the component updated
+    fixture.detectChanges();
+    //we check our DOM element
+    const tabs = el.queryAll(By.css('.mat-tab-label'));
+    //console.log(tabs.length);
+    expect(tabs.length).toBe(1, 'Unexpected number of tabs found');
   });
 
 
   it("should display only advanced courses", () => {
 
-      pending();
+    //we use the 'of' operator of rxjs to let know that the expected response is an Observable<Course[]>
+    coursesService.findAllCourses.and.returnValue(of(advancedCourses));
+    //we let know the component that the data is passed and the component updated
+    fixture.detectChanges();
+    //we check our DOM element
+    const tabs = el.queryAll(By.css('.mat-tab-label'));
+    //console.log(tabs.length);
+    expect(tabs.length).toBe(1, 'Unexpected number of tabs found');
 
   });
 
 
   it("should display both tabs", () => {
 
-    pending();
+    //we use the 'of' operator of rxjs to let know that the expected response is an Observable<Course[]>
+    coursesService.findAllCourses.and.returnValue(of(setupCourses()));
+    //we let know the component that the data is passed and the component updated
+    fixture.detectChanges();
+    //we check our DOM element
+    const tabs = el.queryAll(By.css('.mat-tab-label'));
+    //console.log(tabs.length);
+    expect(tabs.length).toBe(2, 'Expected to find 2 tabs found');
 
   });
 
 
-  it("should display advanced courses when tab clicked", () => {
+  it("should display advanced courses when tab clicked", (done: DoneFn) => {
 
-    pending();
+    //we use the 'of' operator of rxjs to let know that the expected response is an Observable<Course[]>
+    coursesService.findAllCourses.and.returnValue(of(setupCourses()));
+    //we let know the component that the data is passed and the component updated
+    fixture.detectChanges();
+    //we check our DOM element
+    const tabs = el.queryAll(By.css('.mat-tab-label'));
+    //Click on tab simulated
+    //el.nativeElement.click();
+    //we use the helper function click and passed the second tab as an argument
+    click(tabs[1]);
+    //console.log(tabs[1].nativeElement.textContent);
+    //update on the component changes
+    fixture.detectChanges();
+    //Since the component is using an animiation that relies on window.requestAnimationFramte() we get an asynchronous operation, we use a setTimeout and the done parameter to indicate that after that time the assertions should be checked and by running done() we let know that the test is completed
+    setTimeout(() => {
+      const cardTitles = el.queryAll(By.css('.mat-tab-body-active .mat-card-title'));
+      expect(cardTitles.length).toBeGreaterThan(0, 'Could not find card title');
+      //console.log(cardTitles[0].nativeElement.textContent);
+      expect(cardTitles[0].nativeElement.textContent).toContain('Angular Security Course');
 
+      done();
+    }, 500);
   });
 
 });
