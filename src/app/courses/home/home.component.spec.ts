fd19fef2ed94dbaf01f3248cb8c08aff3662c9f0
Janio Isacura <janioisacura@gmail.com>
2022-08-12 17:43:31 -0300

    new UT examples added to home component

--- src/app/courses/home/home.component.spec.ts
+++ src/app/courses/home/home.component.spec.ts
@@ -1,4 +1,4 @@
-import {async, ComponentFixture, fakeAsync, flush, flushMicrotasks, TestBed, waitForAsync} from '@angular/core/testing';
+import {async, ComponentFixture, fakeAsync, flush, flushMicrotasks, TestBed, tick, waitForAsync} from '@angular/core/testing';
 import {CoursesModule} from '../courses.module';
 import {DebugElement} from '@angular/core';
 
@@ -16,12 +16,13 @@ import {click} from '../common/test-utils';
 
 
 
-describe('HomeComponent', () => {
+fdescribe('HomeComponent', () => {
 
   let fixture: ComponentFixture<HomeComponent>;
   let component:HomeComponent;
   let el: DebugElement;
   let coursesService: any;
+  let timeout = 1000;
 
   //we call the findAllCourses method, it returns an Observable<Course[]>, and we mock the result by using the setupCourses() utility and filter the response by category
   const beginnerCourses = setupCourses().filter(course => course.category === 'BEGINNER');
@@ -100,7 +101,7 @@ describe('HomeComponent', () => {
   });
 
 
-  it("should display advanced courses when tab clicked", (done: DoneFn) => {
+  it("should display advanced courses when tab clicked using done()", (done: DoneFn) => {
 
     //we use the 'of' operator of rxjs to let know that the expected response is an Observable<Course[]>
     coursesService.findAllCourses.and.returnValue(of(setupCourses()));
@@ -123,8 +124,57 @@ describe('HomeComponent', () => {
       expect(cardTitles[0].nativeElement.textContent).toContain('Angular Security Course');
 
       done();
-    }, 500);
+    }, timeout);
   });
+
+  it("should display advanced courses when tab clicked - fakeAsync", fakeAsync(() => {
+
+    coursesService.findAllCourses.and.returnValue(of(setupCourses()));
+
+    fixture.detectChanges();
+
+    const tabs = el.queryAll(By.css('.mat-tab-label'));
+
+    click(tabs[1]);
+
+    fixture.detectChanges();
+
+    //we flush the microtask and macrotasks queues before the assertions
+    flush();
+
+    const cardTitles = el.queryAll(By.css('.mat-tab-body-active .mat-card-title'));
+
+    expect(cardTitles.length).toBeGreaterThan(0, 'Could not find card title');
+
+    expect(cardTitles[0].nativeElement.textContent).toContain('Angular Security Course');
+
+  }));
+
+  fit("should display advanced courses when tab clicked - waitForAsync", waitForAsync(() => {
+    //waitForAsync is an alternative but far less flexible than fakeAsync since we dont have control of the microtasks and/or macrotasks execution and we don't have the possibility to write our assertions on a synchronous looking way, but async supports http requests whic is a better option to use it on our TestBed configuration module or in cases when the data is fetched from a backend
+    coursesService.findAllCourses.and.returnValue(of(setupCourses()));
+
+    fixture.detectChanges();
+
+    const tabs = el.queryAll(By.css('.mat-tab-label'));
+
+    click(tabs[1]);
+
+    fixture.detectChanges();
+
+    fixture.whenStable().then(() => {
+      //whenStable() Get a promise that resolves when the fixture is stable.this can be used to resume testing after events have triggered asynchronous activity or asynchronous change detection.
+
+      //console.log('called when stable');
+
+      const cardTitles = el.queryAll(By.css('.mat-tab-body-active .mat-card-title'));
+
+      expect(cardTitles.length).toBeGreaterThan(0, 'Could not find card title');
+
+      expect(cardTitles[0].nativeElement.textContent).toContain('Angular Security Course');
+    });
+
+  }));
 
 });
 
